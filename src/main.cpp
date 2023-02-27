dd6a112d9a817673d2cd4020668c7136316776cb
djw319 <64276595+djw319@users.noreply.github.com>
2023-02-27 14:24:06 -0500

    Change CAN to TWAI

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <driver/gpio.h>
-#include <driver/can.h>
+#include <driver/twai.h>
 
 
 //IO Variables
@@ -22,23 +22,23 @@ unsigned int lastMeasuredTimeBuffer = lastMeasuredTime;
 unsigned int currentTime = 0;
 
 
-void setup_can_driver(){      //Function for setting up CAN driver
-  can_general_config_t general_config={
-    .mode = CAN_MODE_NORMAL,
+void setup_twai_driver(){      //Function for setting up CAN driver
+  twai_general_config_t general_config={
+    .mode = TWAI_MODE_NORMAL,
     .tx_io = (gpio_num_t)GPIO_NUM_5,
     .rx_io = (gpio_num_t)GPIO_NUM_4,
-    .clkout_io = (gpio_num_t)CAN_IO_UNUSED,
-    .bus_off_io = (gpio_num_t)CAN_IO_UNUSED,
+    .clkout_io = (gpio_num_t)TWAI_IO_UNUSED,
+    .bus_off_io = (gpio_num_t)TWAI_IO_UNUSED,
     .tx_queue_len = 0,     //0 for non transmitting
     .rx_queue_len = 65,
-    .alerts_enabled = CAN_ALERT_ALL,
+    .alerts_enabled = TWAI_ALERT_ALL,
     .clkout_divider = 0
   
   };
 
-  can_timing_config_t timing_config = CAN_TIMING_CONFIG_1000KBITS();   //Set Bus Speed to 1000 kbit/s
-  can_filter_config_t filter_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
-
+  twai_timing_config_t timing_config = TWAI_TIMING_CONFIG_500KBITS();   //Set Bus Speed to 1000 kbit/s
+  twai_filter_config_t filter_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+}
 
 void pulseEvent(){
   pulsePeriod = esp_timer_get_time() - lastMeasuredTime;
@@ -55,6 +55,9 @@ void setup() {
   pinMode(tachPin,INPUT);
   pinMode(ledPin,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(tachPin), pulseEvent, FALLING);
+
+  //CAN Setup
+  setup_twai_driver();
 
   //Delay so no negative values occur right at startup
   delay(1000);
@@ -73,6 +76,22 @@ void loop() {
     zeroDebounceExtra = 2000;                         // Change the threshold a little so it doesn't bounce
   }else{
     zeroDebounceExtra = 0;                            // Reset the threshold to the normal value so it doesn't bounce
+  }
+
+  //Configure message to transmit
+  twai_message_t message;
+  message.identifier = 0xC8;
+  //message.flags = TWAI_MSG_FLAG_EXTD;
+  message.data_length_code = 2;
+  for (int i = 0; i < 4; i++) {
+      message.data[i] = 0;
+  }
+
+  //Queue message for transmission
+  if (twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK) {
+      printf("Message queued for transmission\n");
+  } else {
+      printf("Failed to queue message for transmission\n");
   }
 
 
